ca84c3ea2ff3db71154c7e741e563c9db131a1bd
wmacnair <32297936+wmacnair@users.noreply.github.com>
2020-07-15 12:39:19 +0200

    Give get_outliers an exc_groups argument
    Allows users to remove an entire group of samples which are poor quality

--- R/SampleQC_setters_getters.R
+++ R/SampleQC_setters_getters.R
@@ -28,6 +28,7 @@ get_n_groups <- function(qc_obj) {
 #' Extracts list of outliers
 #' 
 #' @param qc_obj Output from fit_sampleQC
+#' @param exc_groups List of sample groups to exclude
 #' 
 #' @importFrom assertthat assert_that
 #' @importFrom magrittr "%>%"
@@ -36,15 +37,24 @@ get_n_groups <- function(qc_obj) {
 #' @return \code{data.table} containing cell_id, sample_id and whether outlier 
 #' or not
 #' @export
-get_outliers <- function(qc_obj) {
+get_outliers <- function(qc_obj, exc_groups=NULL) {
     # check ok
     assert_that( .check_is_qc_obj(qc_obj) == 'fit',
         msg='fit_sampleQC must be run before outliers can be returned')
+    assert_that( all(exc_groups %in% metadata(qc_obj)$group_list),
+        msg='invalid sample groups specified for exc_groups')
 
-    # extract outliers
+    # extract cell outliers
     outliers_dt     = colData(qc_obj)$outlier %>% 
         rbindlist %>%
-        .[, .(sample_id, cell_id, outlier)]
+        .[, .(sample_id, cell_id, out_cell=outlier)]
+
+    # exclude specified samples
+    exc_samples     = qc_obj$sample_id[qc_obj$group_id %in% exc_groups]
+    outliers_dt[, out_sample    := sample_id %in% exc_samples]
+
+    # combine
+    outliers_dt[, outlier       := or(out_cell, out_sample) ]
 
     return(outliers_dt)
 }
