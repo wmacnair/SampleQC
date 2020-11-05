76761537ba5309b8d84dc6578b41f8342a76cd0a
wmacnair <32297936+wmacnair@users.noreply.github.com>
2020-11-05 17:09:37 +0100

    Fix bug in umap for small sample numbers

--- R/SampleQC_samples.R
+++ R/SampleQC_samples.R
@@ -368,7 +368,8 @@ calculate_sample_to_sample_MMDs <- function(qc_dt, qc_names=c('log_counts',
     #         ) %>% t
     #     )
     # umap_mat    = umap(NULL, nn_method=nn_method, min_dist=0.001)
-    umap_mat    = umap(as.dist(mmd_mat), min_dist=0.001)
+    umap_mat    = umap(as.dist(mmd_mat), 
+        n_neighbors=n_nhbrs, min_dist=0.001)
 
     # tidy up
     umap_mat    = apply(umap_mat, 2, rescale, to=c(0.05, 0.95))
